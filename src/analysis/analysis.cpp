cf2615988b3dd3d46ebb4da87e82c76792287ec9
LuisHDBueno <105614892+LuisHDBueno@users.noreply.github.com>
2023-06-29 11:06:51 -0300

    Crie os arquivos bases para a analise da arvore

---
+++ src/analysis/analysis.cpp
@@ -0,0 +1,11 @@
+/**
+ * Este arquivo 'source' é parte do projeto {{ A2_Estrutura_de_Dados }}.
+ * 
+ * Tem como objetivo guardar as declarações de funções e estruturas necessárias para análise de árvores binárias de busca.
+*/
+
+#include <iostream>
+#include "analysis.h"
+
+using namespace std;
+
---
+++ test/test_analysis.cpp
@@ -0,0 +1,25 @@
+/**
+ * Este arquivo 'driver code' é parte secundária do projeto {{ A2_Estrutura_de_Dados }}.
+ * 
+ * Tem como objetivo ser compilado junto aos arquivos tree.cpp e user.cpp, encontrados na pasta \src\tree e \src\user do projeto,
+ * e realiza uma série de testes nas funções ali designadas.
+ * 
+ * Para rodar esse arquivo executar no prompt de comando:
+ * g++ -o test_analysis.exe .\test_analysis.cpp ..\src\tree\tree.cpp ..\src\analysis\analysis.cpp
+*/
+
+#include <iostream>
+#include <windows.h>
+#include "..\src\tree\tree.h"
+#include "..\src\analysis\analysis.h"
+
+#pragma execution_character_set( "utf-8" )
+
+using namespace std;
+
+int main()
+{
+
+    
+
+}
