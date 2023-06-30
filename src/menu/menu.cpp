8e395e93e838cd818dc2e58ff928a010f07b20fe
Anajuliarlc <anajuliaritalc@gmail.com>
2023-06-30 18:36:52 -0300

    Coloque o tempo de processamento

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -1,9 +1,10 @@
-/* g++ -o menu.exe .\menu.cpp ..\tree\tree.cpp ..\analysis\analysis.cpp ..\user\user.cpp ..\list\list.cpp 
+/*
+ *g++ -o menu.exe .\menu.cpp ..\tree\tree.cpp ..\analysis\analysis.cpp ..\user\user.cpp ..\list\list.cpp 
 */
-
 
 #include <iostream>
 #include <windows.h>
+#include <chrono>
 #include "menu.h"
 #include "..\tree\tree.h"
 #include "..\analysis\analysis.h"
@@ -18,6 +19,9 @@ void menuAnalysis(struct TreeNode* ptrTree)
 { 
     SetConsoleOutputCP( 65001 );
     char cOption;
+    // Variáveis para medir o tempo de execução
+    auto start = chrono::high_resolution_clock::now();
+    auto end = chrono::high_resolution_clock::now();
 
     // Limpar a tela
     while(true)
@@ -25,41 +29,57 @@ void menuAnalysis(struct TreeNode* ptrTree)
         cout << "\033[2J\033[1;1H";
 
         cout << "========== Menu ==========" << endl;
-        cout << "1. Qual a altura da árvore?" << endl;
-        cout << "2. Qual é o tamanho da árvore?" << endl;
-        cout << "3. Gostaria de inserir um elemento na sua árvore?" << endl;
-        cout << "4. Gostaria de deletar um elemento na sua árvore?" << endl;
-        cout << "5. Gostaria de buscar o endereço de memória de um elemento da árvore?" << endl;
-        cout << "6. Sua árvore é completa?" << endl;
-        cout << "7. Sua árvore é perfeita?" << endl;
-        cout << "8. Exiba sua árvore em BFS aqui" << endl;
-        cout << "9. Ordene sua árvore com Bubble Sort" << endl;
-        cout << "10. Ordene sua árvore com Selection Sort" << endl;
-        cout << "11. Ordene sua árvore com Insertion Sort" << endl;
-        cout << "12. Ordene sua árvore com Shell Sort" << endl;
-        cout << "13. Volte ao menu anterior" << endl;
+        cout << "a. Qual a altura da árvore?" << endl;
+        cout << "b. Qual é o tamanho da árvore?" << endl;
+        cout << "c. Gostaria de inserir um elemento na sua árvore?" << endl;
+        cout << "d. Gostaria de deletar um elemento na sua árvore?" << endl;
+        cout << "e. Gostaria de buscar o endereço de memória de um elemento da árvore?" << endl; 
+        cout << "f. Sua árvore é completa?" << endl;
+        cout << "g. Sua árvore é perfeita?" << endl;
+        cout << "h. Exiba sua árvore em BFS aqui" << endl;
+        cout << "i. Ordene sua árvore com Bubble Sort" << endl;
+        cout << "j. Ordene sua árvore com Selection Sort" << endl;
+        cout << "k. Ordene sua árvore com Insertion Sort" << endl;
+        cout << "l. Ordene sua árvore com Shell Sort" << endl;
+        cout << "m. Volte ao menu anterior" << endl;
         cout << "==========================" << endl;
         cout << "Escolha uma opção: ";
 
         cin >> cOption;
         switch(cOption)
         {
-            case '1':
+            case 'a':
+                start = chrono::high_resolution_clock::now();
                 cout << "A altura da árvore é: " << getHeight(ptrTree) << endl;
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '2':
+            case 'b':
+                start = chrono::high_resolution_clock::now();
                 cout << "O tamanho da árvore é: " << getSize(ptrTree) << endl;
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '3':
+            case 'c':
+                start = chrono::high_resolution_clock::now();
                 ptrTree = userInsertNode(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '4':
+            case 'd':
+                start = chrono::high_resolution_clock::now();
                 ptrTree = userDeleteNode(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '5':
-                userSearchNode(ptrTree);
+            case 'e':
+                start = chrono::high_resolution_clock::now();
+                cout << userSearchNode(ptrTree)<< endl;
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '6':
+            case 'f':
+                start = chrono::high_resolution_clock::now();
                 if (isComplete(ptrTree))
                 {
                     cout << "A árvore é completa!" << endl;
@@ -68,8 +88,11 @@ void menuAnalysis(struct TreeNode* ptrTree)
                 {
                     cout << "A árvore não é completa!" << endl;
                 }
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '7':
+            case 'g':
+                start = chrono::high_resolution_clock::now();
                 if (isPerfect(ptrTree, getHeight(ptrTree), 0))
                 {
                     cout << "A árvore é perfeita!" << endl;
@@ -78,23 +101,40 @@ void menuAnalysis(struct TreeNode* ptrTree)
                 {
                     cout << "A árvore não é perfeita!" << endl;
                 }
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '8':
+            case 'h':
+                start = chrono::high_resolution_clock::now();
                 printBFS(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '9':
+            case 'i':
+                start = chrono::high_resolution_clock::now();
                 bubbleSortTree(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '10':
+            case 'j':
+                start = chrono::high_resolution_clock::now();
                 selectionSortTree(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '11':
+            case 'k':
+                start = chrono::high_resolution_clock::now();
                 insertionSortTree(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '12':
+            case 'l':
+                start = chrono::high_resolution_clock::now();
                 shellSortTree(ptrTree);
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            case '13':
+            case 'm':
                 cout << "Voltando..." << endl;
                 return;
             default:
@@ -115,6 +155,9 @@ void menuConstruction()
     char cOption;
     TreeNode* ptrTree = nullptr;
     SetConsoleOutputCP( 65001 );
+    auto start = chrono::high_resolution_clock::now();
+    auto end = chrono::high_resolution_clock::now();
+
     while (true) 
     {
         
@@ -134,29 +177,33 @@ void menuConstruction()
         switch (cOption) 
         {
             case '1':
-            if (ptrTree != nullptr)
-            {
-                deleteTree(&ptrTree, ptrTree);
-                ptrTree = userReadFile();
+                start = chrono::high_resolution_clock::now();
+                if (ptrTree != nullptr)
+                {
+                    deleteTree(&ptrTree, ptrTree);
+                    ptrTree = userReadFile();
+                }
+                else
+                {
+                    ptrTree = userReadFile();
+                }
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << " nanosegundos" << endl;
                 break;
-            }
-            else
-            {
-                ptrTree = userReadFile();
-                break;
-            }
             case '2':
+                start = chrono::high_resolution_clock::now();
                 if (ptrTree != nullptr)
                 {
                     deleteTree(&ptrTree, ptrTree);
                     ptrTree = userCreateTree();
-                    break;
                 }
                 else
                 {
                     ptrTree = userCreateTree();
-                    break;
                 }
+                end = chrono::high_resolution_clock::now();
+                cout << "Tempo de execução: " << chrono::duration_cast<chrono::seconds>(end - start).count() << " segundos" << endl;
+                break;
             case '3':
                 if (ptrTree == nullptr)
                 {
